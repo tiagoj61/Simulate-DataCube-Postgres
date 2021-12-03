e76354d39bfe5f0e76d6ef04733c213ffb851fcc
Tiago Moraes <62123056+tiagoj61@users.noreply.github.com>
2021-12-03 05:42:01 -0300

    correct

--- functions/Array_Remove_Element_By_Index.sql
+++ functions/Array_Remove_Element_By_Index.sql
@@ -1,3 +1,3 @@
-CREATE OR REPLACE FUNCTION array_remove_element_by_index(anyarray, int)
+CREATE OR REPLACE FUNCTION remover_elemento_no_index(anyarray, int)
   RETURNS anyarray LANGUAGE sql IMMUTABLE AS
 'SELECT $1[1:$2-1] || $1[$2+1:2147483647]';
